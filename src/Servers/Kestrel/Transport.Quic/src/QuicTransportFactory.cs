904a57c549234b860651487d53d90f022fae16f9
James Newton-King <james@newtonking.com>
2022-07-19 15:41:12 +0800

    Fix build

--- src/Servers/Kestrel/Transport.Quic/src/QuicTransportFactory.cs
+++ src/Servers/Kestrel/Transport.Quic/src/QuicTransportFactory.cs
@@ -1,9 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Linq;
 using System.Net;
-using System.Net.Security;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Server.Kestrel.Transport.Quic.Internal;
